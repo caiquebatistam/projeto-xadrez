96e379ffdaaf3fe03bcfb3efcdba632e2d526d4f
caiquebatistam <88943229+caiquebatistam@users.noreply.github.com>
2022-06-05 12:46:39 -0300

    Adicionando classe de posição da peça

---
+++ projeto-xadrez/src/boardgame/Position.java
@@ -0,0 +1,54 @@
+package boardgame;
+
+
+/**
+ * 
+ * @author Caique.Batista
+ *
+ */
+public class Position {
+	
+	private int row;
+	
+	private int column;
+
+	
+	//Construtor
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+
+	
+
+	
+	public int getRow() {
+		return row;
+	}
+
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+
+	public int getColumn() {
+		return column;
+	}
+
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return row + "," + column;
+	}
+	
+	
+	
+
+}
